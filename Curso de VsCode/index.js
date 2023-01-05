57b789325f4a1800089b6e170d2693ddcfc2219f
Jose Luis Gonzalez Tirado <121734578+ProgramadorAlpha@users.noreply.github.com>
2023-01-05 22:33:59 +0000

    Add files via upload

---
+++ Curso de VsCode/index.js
@@ -0,0 +1,14 @@
+const fn = () => {
+    const miString = "soy un string!"
+    let b = miString + "muy feliz"
+    console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "chanchito feliz"
+
+    console.log(a)
+}
+
