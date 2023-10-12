1f9eb3ea7a5451712ef590c0c84aa563f15da14a
Jose Luis Gonzalez Tirado <121734578+ProgramadorAlpha@users.noreply.github.com>
2023-10-12 18:27:10 +0100

    Update 005Operadores_y_TiposdeDatos.js

--- Master en JavaScript,JQuery,Angular,NodeJs/JavaScript/005Operadores_y_TiposdeDatos.js
+++ Master en JavaScript,JQuery,Angular,NodeJs/JavaScript/005Operadores_y_TiposdeDatos.js
@@ -29,7 +29,7 @@ var numero_falso = "33";
 
 console.log(Number(numero_falso)+7);  //Aqui el resultado en la consola va a ser 40 ya que 33 se conivirtio en un numero operable, osea un Entero.... 
 
-//Tambien estan las funciones: parseInt y parseFloat para numeros decimales. Ademas del String para covertir a texto por ejemplo un numero_entero
+//Tambien estan las funciones: parseInt para enteros tambien y parseFloat para numeros decimales. Ademas del String para covertir a texto por ejemplo un numero_entero
 //Por ultimo en esta leccion tenemos a  typeof esto el console.log(''); nos va a detectar cualquier tipo de dato ejemplo:
 
 console.log(typeof numero_entero);
